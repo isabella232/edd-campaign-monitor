bd23c7dc6f349e838f2b9c31650435e2ad6935f4
Ashley Gibson <ashley@nosegraze.com>
2020-10-15 15:11:45 +0100

    Change since version number.

--- edd-campaign-monitor.php
+++ edd-campaign-monitor.php
@@ -31,7 +31,7 @@ if( class_exists( 'EDD_License' ) ) {
  * Registers the subsection for EDD Settings.
  *
  * @access public
- * @since  1.2
+ * @since  1.1.2
  *
  * @param  array $sections Settings Sections.
  *
